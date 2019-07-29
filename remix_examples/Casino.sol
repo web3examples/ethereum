c993031f1cf46a69cbb3ee325ecdd351d5fef4f5
gpersoon <mail@gpersoon.com>
2019-07-29 15:37:13 +0200

    Update Casino.sol

--- remix_examples/Casino.sol
+++ remix_examples/Casino.sol
@@ -1,36 +1,44 @@
+// Load in remix: remix.loadurl("https://github.com/web3examples/ethereum/remix_examples/Casino.sol")
 pragma solidity >=0.4.0 <0.7.0;
 
-/// @author Gerard Persoon
-/// @title A simple casino
+/** @author Gerard Persoon
+    @title A simple casino
+*/
 contract Casino {
 
     /// Setup an intial amount for the bank, supplied during the creation of the contract.    
     constructor() public payable {
     }
 
-    /// Draw a random number
-    /// @dev this is not secure but only to demonstrate
+    /** Draw a random number
+        @dev this is not secure but only to demonstrate
+        @return a pseudo random number
+    */
     function getRandom()  public view returns(uint256) { 
         return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, now)));  
     }    
 
         
-    /// Check the balance of the bank
+    /** Check the balance of the bank
+        @return the balance
+    */
     function bankBalance() public view returns(uint256 ret) {
         return address(this).balance;
     }
 
-    /// Perform the bet and pay out if you win
-    /// @dev several temporary variabled are you to make debugging easier
+    /** Perform the bet and pay out if you win
+        @dev several temporary variabled are created to make debugging easier
+    /*
     function betAndWin() public payable returns(bool win)  {
-        address betPlacer = address(msg.sender);
+        address payable betPlacer = address(msg.sender);
         uint bet = msg.value;
+        uint payout = bet * 2;
         uint balance = bankBalance();    
         require(bet >0        ,"No money added to bet.");
-        require(bet <= balance, "Not enough money in bank for this bet.");
+        require(payout <= balance, "Not enough money in bank for this bet."); // bet has already been added
         win = bool (getRandom() % 2 == 0 );
         if (win)
-            betPlacer.transfer(2*bet);
+            betPlacer.transfer(payout);
         return (win);
     }
 }

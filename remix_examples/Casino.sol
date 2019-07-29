0a41c5c800f2d7cadfb4e54c06bdd01980952044
gpersoon <mail@gpersoon.com>
2019-07-29 13:38:40 +0200

    Update Casino.sol

--- remix_examples/Casino.sol
+++ remix_examples/Casino.sol
@@ -1,28 +1,36 @@
 pragma solidity >=0.4.0 <0.7.0;
+
+/// @author Gerard Persoon
+/// @title A simple casino
 contract Casino {
-    
-constructor() public  payable {
-}
-    
-function getRandom( )  public view returns(uint256) { 	// DISCLAIMER: Not secure // https://blog.positive.com/predicting-random-numbers-in-ethereum-smart-contracts-e5358c6b8620
-    return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, now)));  
-}    
 
+    /// Setup an intial amount for the bank, supplied during the creation of the contract.    
+    constructor() public payable {
+    }
 
-function bankBalance() public view returns(uint256 ret) {
-    return address(this).balance;
-}
+    /// Draw a random number
+    /// @dev this is not secure but only to demonstrate
+    function getRandom()  public view returns(uint256) { 
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, now)));  
+    }    
 
+        
+    /// Check the balance of the bank
+    function bankBalance() public view returns(uint256 ret) {
+        return address(this).balance;
+    }
 
-function betAndWin() public payable returns(bool win)  {
-    require(msg.value >0,"No money added to bet.");
-    uint bet = msg.value;
-    uint balance = bankBalance();
-    require(balance >= bet, "Not enough money in bank for this bet.");
-    win = bool (getRandom() % 2 == 0 );
-    if (win)
-        address(msg.sender).transfer(2*bet);
-    return (win);
-}
-
+    /// Perform the bet and pay out if you win
+    /// @dev several temporary variabled are you to make debugging easier
+    function betAndWin() public payable returns(bool win)  {
+        address betPlacer = address(msg.sender);
+        uint bet = msg.value;
+        uint balance = bankBalance();    
+        require(bet >0        ,"No money added to bet.");
+        require(bet <= balance, "Not enough money in bank for this bet.");
+        win = bool (getRandom() % 2 == 0 );
+        if (win)
+            betPlacer.transfer(2*bet);
+        return (win);
+    }
 }

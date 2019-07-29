c7cad1f696f5dd89c08aa7df6a3c7eab543b79b9
gpersoon <mail@gpersoon.com>
2019-07-29 15:56:31 +0200

    Update Casino.sol

--- remix_examples/Casino.sol
+++ remix_examples/Casino.sol
@@ -10,35 +10,36 @@ contract Casino {
     constructor() public payable {
     }
 
-    /** Draw a random number
-        @dev this is not secure but only to demonstrate
-        @return a pseudo random number
-    */
-    function getRandom()  public view returns(uint256) { 
-        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, now)));  
-    }    
-
-        
-    /** Check the balance of the bank
-        @return the balance
-    */
-    function bankBalance() public view returns(uint256 ret) {
-        return address(this).balance;
-    }
-
     /** Perform the bet and pay out if you win
         @dev several temporary variabled are created to make debugging easier
-    /*
+    */
     function betAndWin() public payable returns(bool win)  {
         address payable betPlacer = address(msg.sender);
         uint bet = msg.value;
         uint payout = bet * 2;
-        uint balance = bankBalance();    
-        require(bet >0        ,"No money added to bet.");
-        require(payout <= balance, "Not enough money in bank for this bet."); // bet has already been added
-        win = bool (getRandom() % 2 == 0 );
+        uint balance = getBankBalance();    
+        require(bet > 0, "No money added to bet.");
+        require(payout <= balance, "Not enough money in bank for this bet."); // bet has already been added to bank balance
+        win = bool (getRandom()%2 == 0);
         if (win)
             betPlacer.transfer(payout);
         return (win);
     }
+
+    /** Check the balance of the bank
+        @return the balance
+    */
+    function getBankBalance() public view returns(uint256 ret) {
+        return address(this).balance;
+    }
+    
+    /** Draw a random number
+        @dev this is not secure but only to demonstrate
+        @return a pseudo random number
+    */
+    function getRandom() public view returns(uint256) { 
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, block.timestamp)));  
+    }    
+
+
 }

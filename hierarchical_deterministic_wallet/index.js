3dbe320995b944de3d817b49ecbbc78800f1ab6b
gpersoon <mail@gpersoon.com>
2019-08-15 16:22:20 +0200

    Update index.js

--- hierarchical_deterministic_wallet/index.js
+++ hierarchical_deterministic_wallet/index.js
@@ -1,26 +1,35 @@
 const Web3 = require('web3');
 const web3 = new Web3();
 const bip39 = require('bip39')
-var HDKey = require('hdkey')
-
+const HDKey = require('hdkey')
 const mnemonic="post soda ozone trash forget egg regret wink length minor winner broken";
 console.log(`Starting with mnemonic: ${mnemonic}`);
 const seed=bip39.mnemonicToSeedSync(mnemonic)
 const hdWallet = HDKey.fromMasterSeed(seed);
 
+var wallet=hdWallet;
+console.log(`Masterkey: ${'0x'+wallet.privateKey.toString('hex')}`);
 
-var wallet=hdWallet;                        console.log(`Masterkey: ${'0x'+wallet.privateKey.toString('hex')}`);
-wallet = wallet.deriveChild(0x80000000+44); console.log(`Derive 44': ${'0x'+wallet.privateKey.toString('hex')}`);
-wallet = wallet.deriveChild(0x80000000+60); console.log(`Derive 60': ${'0x'+wallet.privateKey.toString('hex')}`);
-wallet = wallet.deriveChild(0x80000000+0);  console.log(`Derive  0': ${'0x'+wallet.privateKey.toString('hex')}`);
-wallet = wallet.deriveChild(0);             console.log(`Derive  0 : ${'0x'+wallet.privateKey.toString('hex')}`);
-wallet = wallet.deriveChild(0);             console.log(`Derive  0 : ${'0x'+wallet.privateKey.toString('hex')}`);
+wallet = wallet.deriveChild(0x80000000+44);
+console.log(`Derive 44': ${'0x'+wallet.privateKey.toString('hex')}`);
+
+wallet = wallet.deriveChild(0x80000000+60);
+console.log(`Derive 60': ${'0x'+wallet.privateKey.toString('hex')}`);
+
+wallet = wallet.deriveChild(0x80000000+0);
+console.log(`Derive  0': ${'0x'+wallet.privateKey.toString('hex')}`);
+
+wallet = wallet.deriveChild(0);
+console.log(`Derive  0 : ${'0x'+wallet.privateKey.toString('hex')}`);
+
+wallet = wallet.deriveChild(0);
+console.log(`Derive  0 : ${'0x'+wallet.privateKey.toString('hex')}`);
 
 const root = hdWallet.derive("m/44'/60'/0'/0/0")
 console.log(`Direct derive m/44'/60'/0'/0/0: ${'0x'+root._privateKey.toString('hex')}`);
 
 var account=web3.eth.accounts.privateKeyToAccount("0x"+root.privateKey.toString('hex'));
-console.log(`Correspinding account: ${account.address}`);
+console.log(`Corresponding account: ${account.address}`);
 
 // priv key: 0x04BFCEDBBAA686F15643DB581857BF06CE19830D10CBA4EBF4B35899F1410AD4
 // account:  0x6c728716a68499d486cDA1701AB13C7b57f30aA0

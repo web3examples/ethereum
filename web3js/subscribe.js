8c509288610b678b3b1060285094c30588706994
Gerard Persoon <mail@gpersoon.com>
2019-11-27 10:32:13 +0100

    Update subscribe.js

--- web3js/subscribe.js
+++ web3js/subscribe.js
@@ -1,7 +1,7 @@
 const Web3 = require('web3');
-const web3 = new Web3("wss://ropsten.infura.io/ws/v3/40548c5748a04570b1859a7bd8f9e615" );
+const web3 = new Web3("wss://mainnet.infura.io/ws");
 
 var subscription= web3.eth.subscribe('logs', {fromBlock: '6852000'} )
     .on("data", console.log )
     .on("changed", console.log)
-    .on("error",console.log);
+    .on("error",console.log)

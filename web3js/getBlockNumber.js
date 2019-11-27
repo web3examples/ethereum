aa8de8a3decbc425e4a32ba2772833641a63881d
Gerard Persoon <mail@gpersoon.com>
2019-11-27 10:40:08 +0100

    Update getBlockNumber.js

--- web3js/getBlockNumber.js
+++ web3js/getBlockNumber.js
@@ -1,5 +1,13 @@
-Web3 = require('web3');
-var web3 = new Web3( "https://mainnet.infura.io");
-console.log(web3.version);
-web3.eth.getBlockNumber()
-    .then(console.log);
+const Web3 = require('web3');
+const web3 = new Web3("wss://ropsten.infura.io/ws");
+
+async function f() {
+   var currentBlockNumber=await web3.eth.getBlockNumber();
+console.log(currentBlockNumber);
+
+var subscription= web3.eth.subscribe('logs', {fromBlock: currentBlockNumber} )
+    .on("data", console.log )
+    .on("changed", console.log)
+    .on("error",console.log)
+}
+f();

04c062f70613d8a75f0f0edef2604fd10f0cd740
Gerard Persoon <mail@gpersoon.com>
2019-11-27 10:30:31 +0100

    Update subscribe.js

--- web3js/subscribe.js
+++ web3js/subscribe.js
@@ -4,14 +4,4 @@ const web3 = new Web3("wss://ropsten.infura.io/ws/v3/40548c5748a04570b1859a7bd8f
 var subscription= web3.eth.subscribe('logs', {fromBlock: '6852000'} )
     .on("data", console.log )
     .on("changed", console.log)
-    .on("error",console.log)    
-
-console.log('Press key & enter to exit');
-process.stdin.setRawMode=true
-process.stdin.resume();
-process.stdin.on('data', () =>
-{   console.log("Stopping"); // should unsubscribe
-    process.exit(0);
-});
-
-
+    .on("error",console.log);

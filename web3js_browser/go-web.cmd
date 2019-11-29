30c5b67cac26995c5e88577217870bf949755db0
gpersoon <mail@gpersoon.com>
2019-11-29 15:34:14 +0100

    up

--- web3js_browser/go-web.cmd
+++ web3js_browser/go-web.cmd
@@ -1,2 +1,3 @@
-start "" http://127.0.0.1:80
+rem start "" http://127.0.0.1:80
+rem start "%localappdata%\Google\Chrome SxS\Application\chrome.exe" http://127.0.0.1:80
 python -m SimpleHTTPServer 80

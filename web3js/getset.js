e1c2f7f1cdc1b7c838a570d41e24c981a0d0ed74
Gerard Persoon <mail@gpersoon.com>
2019-12-02 13:44:29 +0100

    Update getset.js

--- web3js/getset.js
+++ web3js/getset.js
@@ -1,3 +1,4 @@
+// solidity: https://github.com/web3examples/ethereum/blob/master/solidity_examples/getset.sol
 const Web3 = require('web3');
 const web3 = new Web3('http://localhost:8545' );
 const TestPayABI= [
@@ -79,30 +80,8 @@ const TestPayABI= [
       }
     ];
     
-    
-logABI=[
-          {
-            "indexed": false,
-            "internalType": "string",
-            "name": "message",
-            "type": "string"
-          },
-          {
-            "indexed": false,
-            "internalType": "address",
-            "name": "caller",
-            "type": "address"
-          },
-          {
-            "indexed": false,
-            "internalType": "uint8",
-            "name": "value",
-            "type": "uint8"
-          }
-        ];
-    
 async function f() {      
-    const TestPayaddress="0x4a07a27eF50c2bA0df2e8635A3Ef43E16a391343";
+    const TestPayaddress="0x4a07a27eF50c2bA0df2e8635A3Ef43E16a391343"; // replace with real address
     var acts=await web3.eth.getAccounts();
     const ContractTestPay   = new web3.eth.Contract(TestPayABI, TestPayaddress );
     var result = await ContractTestPay.methods.Set(6).send({from: acts[0]}).catch(console.log);

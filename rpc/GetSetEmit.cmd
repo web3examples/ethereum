52ecae60c734c7f1b788538de5d792da47cf24f9
gpersoon <mail@gpersoon.com>
2019-12-03 15:03:01 +0100

    Update GetSetEmit.cmd

--- rpc/GetSetEmit.cmd
+++ rpc/GetSetEmit.cmd
@@ -1,7 +1,12 @@
+@echo off
 set from=0xEA9a7c7cD8d4Dc3acc6f0AaEc1506C8D6041a1c5
 set to=0x2bD819440B8621214A17e7BCFD70107bBE0A28fC
 set data=0xb1976a02
-rem 0000000000000000000000000000000000000000000000000000000000000000
-curl -H "Content-Type: application/json" -d "{""jsonrpc"":""2.0"",""method"":""eth_call"",""params"":[{""from"":""%from%"",""to"":""%to%"",""data"":""%data%""},""latest""],""id"":67}" https://ropsten.infura.io   
-
+set request=eth_call
+set param={"from":"%from%","to":"%to%","data":"%data%"},"latest"
+set body={"jsonrpc":"2.0","method":"%request%","params":[%param%],"id":67}
+echo curl -H "Content-Type: application/json" -g --data "%body:"=""%"  https://ropsten.infura.io  
+curl -H "Content-Type: application/json" -g --data "%body:"=""%"  https://ropsten.infura.io  
+echo.
 pause
+

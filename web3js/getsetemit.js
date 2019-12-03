8f6caaa2a556b1ddcbe255b5700b8a2a7c0f5fc7
gpersoon <mail@gpersoon.com>
2019-12-03 16:22:08 +0100

    up

--- web3js/getsetemit.js
+++ web3js/getsetemit.js
@@ -1,32 +1,7 @@
-// solidity: https://github.com/web3examples/ethereum/blob/master/solidity_examples/getset.sol
+// solidity: https://github.com/web3examples/ethereum/blob/master/solidity_examples/getsetemit.sol
 const Web3 = require('web3');
 const web3 = new Web3('https://ropsten.infura.io' );
-const TestPayABI= [
-    {
-        "anonymous": false,
-        "inputs": [
-          {
-            "indexed": false,
-            "internalType": "string",
-            "name": "message",
-            "type": "string"
-          },
-          {
-            "indexed": false,
-            "internalType": "address",
-            "name": "caller",
-            "type": "address"
-          },
-          {
-            "indexed": false,
-            "internalType": "uint8",
-            "name": "value",
-            "type": "uint8"
-          }
-        ],
-        "name": "Log",
-        "type": "event"
-      },
+const GetSetEmitABI=[
       {
         "constant": true,
         "inputs": [],
@@ -41,53 +16,14 @@ const TestPayABI= [
         "payable": false,
         "stateMutability": "view",
         "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "internalType": "uint8",
-            "name": "x",
-            "type": "uint8"
-          }
-        ],
-        "name": "Set",
-        "outputs": [
-          {
-            "internalType": "uint8",
-            "name": "",
-            "type": "uint8"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [],
-        "name": "storedData",
-        "outputs": [
-          {
-            "internalType": "uint8",
-            "name": "",
-            "type": "uint8"
-          }
-        ],
-        "payable": false,
-        "stateMutability": "view",
-        "type": "function"
       }
     ];
     
 async function f() {      
-    const TestPayaddress="0x2bD819440B8621214A17e7BCFD70107bBE0A28fC"; // Ropsten address
-    var acts=await web3.eth.getAccounts();
-    const ContractTestPay   = new web3.eth.Contract(TestPayABI, TestPayaddress );
-    var result = await ContractTestPay.methods.Set(6).send({from: acts[0]}).catch(console.log);
-    var decoded=result.events.Log.returnValues;
-    log(`Event emitted: message=${decoded.message} address=${decoded.caller} value=${decoded.value}`);
-    var result = await ContractTestPay.methods.Set(6).call({from: acts[0]}).catch(console.log);
-    log(`Function result (calculated via read only call): ${result}`);
+    const GetSetEmitAddress="0x2bD819440B8621214A17e7BCFD70107bBE0A28fC"; // Ropsten address
+    const acts=await web3.eth.getAccounts();
+    const GetSetEmitContract=new web3.eth.Contract(GetSetEmitABI, GetSetEmitAddress);
+    let result = await GetSetEmitContract.methods.Get().call({from: acts[0]}).catch(console.log);
+    console.log(`Get result: ${result}`);
 }
 f();

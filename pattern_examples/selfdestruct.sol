a472aa82601c316069bebccdf609365edd1de3a7
Gerard Persoon <mail@gpersoon.com>
2019-12-11 11:24:56 +0100

    Update selfdestruct.sol

--- pattern_examples/selfdestruct.sol
+++ pattern_examples/selfdestruct.sol
@@ -2,14 +2,14 @@ pragma solidity ^0.5.12;
 
 contract Mortal {
 
-  string public name="Mortal";
+    string public name="Mortal";
 
-  function destroy() public  { // note add security
-    selfdestruct(msg.sender);
-  }
+    function destroy() public  { // note add security
+        selfdestruct(msg.sender);
+    }
 
-  function destroyAndSend(address payable recipient) public  {
+    function destroyAndSend(address payable recipient) public {
       // note add security
     selfdestruct(recipient);
-  }
+    }
 }

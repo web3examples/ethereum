723c79a94c38d999bffbcf6597beda90cfc8e82b
Gerard Persoon <mail@gpersoon.com>
2020-01-08 11:34:48 +0100

    Create sub.sol

---
+++ test_examples/sub.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.6.1;
+
+library SafeMathSubLib {
+  function sub(uint256 _a, uint256 _b) internal pure returns (uint256) {
+    require(_b <= _a,"a should be larger or equal to b");
+    uint256 c = _a - _b;
+    return c;
+  }
+}
+
+contract SafeMathProxy { // Need external function for try/catch
+    function sub(uint256 _a, uint256 _b) external pure returns (uint256) {
+        return SafeMathSubLib.sub(_a,_b);
+    }
+}

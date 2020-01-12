93ca5a5320180fad4d27f4392e73478a073b7e8c
gpersoon <mail@gpersoon.com>
2020-01-12 14:23:59 +0100

    Update sol6_slice.sol

--- solidity_examples/sol6_slice.sol
+++ solidity_examples/sol6_slice.sol
@@ -4,13 +4,11 @@ contract Slice{
     
     bytes exampleBytes = hex'abcd';
     event logbytes(bytes);
-
     function test(bytes calldata data, uint n) external  {
         bytes memory b=bytes(data[:n]);
         emit logbytes(b);
         
-    }    
-    
+    }        
     function testall() external {
         this.test(exampleBytes,0);
         this.test(exampleBytes,1);

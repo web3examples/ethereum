6b9a4a56c15882219a04764f31c305eef1048846
Jeff Prestes <jeffprestes@gmail.com>
2020-03-30 17:09:23 -0300

    example improvements

--- solidity_examples/sol6_addressexternal.sol
+++ solidity_examples/sol6_addressexternal.sol
@@ -4,9 +4,21 @@ contract A {
     function foo()  pure external returns (string memory) {
         return "A";
     }
+    function getFooAddress() public view returns(address) {
+        return this.foo.address;
+    }
 }
 
 contract B {
    A acontract;
-   address x= acontract.foo.address;
+   address x;
+   
+   constructor(address _addr) public {
+       acontract = A(_addr);
+       x = acontract.foo.address;
+   } 
+   
+   function getAddress() view public returns (address) {
+       return x;
+   }
 }   

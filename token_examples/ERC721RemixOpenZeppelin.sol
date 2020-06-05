a811863695225028f402b0a795fb91865518105f
gpersoon <mail@gpersoon.com>
2020-06-05 18:21:19 +0200

    Update ERC721RemixOpenZeppelin.sol

--- token_examples/ERC721RemixOpenZeppelin.sol
+++ token_examples/ERC721RemixOpenZeppelin.sol
@@ -1,9 +1,9 @@
+// use plenty of gas
 pragma solidity >=0.6.0 <0.7.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 contract Web3Examples721 is ERC721 {
 constructor() ERC721("Web3Examples721", "WEB3E" )  public {  
     _setBaseURI("https://web3examples.com/ethereum/token_examples/");
-    _mint(msg.sender, 1);
-    
+    _mint(msg.sender, 1);    
   }
 }

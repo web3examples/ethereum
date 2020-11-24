487b91a02658a33b299ce32eee9c8dfd6274d157
gpersoon <mail@gpersoon.com>
2020-11-24 10:45:56 +0100

    abi

--- casino/contracts/Casino.sol
+++ casino/contracts/Casino.sol
@@ -6,9 +6,9 @@ pragma solidity >=0.5.0 <0.7.0;
 */
 contract Casino {
 
-    event Won(bool win)  ;   // declaring event  
+    event Won(bool win)  ;   // declaring event
 
-    /// Setup an intial amount for the bank, supplied during the creation of the contract.    
+    /// Setup an intial amount for the bank, supplied during the creation of the contract.
     constructor() public payable {
     }
 
@@ -19,12 +19,12 @@ contract Casino {
         address payable betPlacer = address(msg.sender);
         uint bet = msg.value;
         uint payout = bet * 2;
-        uint balance = getBankBalance();    
+        uint balance = getBankBalance();
         require(bet > 0, "No money added to bet.");
         require(payout <= balance, "Not enough money in bank for this bet."); // bet has already been added to bank balance
         bool win = bool (getRandom()%2 == 0);
         if (win)
-            betPlacer.transfer(payout);        
+            betPlacer.transfer(payout);
         emit Won(win);// logging event
     }
 
@@ -34,18 +34,18 @@ contract Casino {
     function getBankBalance() public view returns(uint256 ret) {
         return address(this).balance;
     }
-    
+
     /** Draw a random number
         @dev this is not secure but only to demonstrate
         @return a pseudo random number
     */
-    function getRandom() public view returns(uint256) { 
-        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, block.timestamp)));  
-    }    
+    function getRandom() public view returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.coinbase, block.timestamp)));
+    }
 
     /** Deposit more funds for bank
         @dev used when the bank runs out of money
     */
-    function () external payable { 
+    function () external payable {
     }
 }

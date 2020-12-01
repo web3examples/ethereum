fe406aacac475e1778e1ae4b9c47e0a28bd45daf
gpersoon <mail@gpersoon.com>
2020-12-01 17:33:30 +0100

    chainlink

--- oracle_examples/chainlink_random.sol
+++ oracle_examples/chainlink_random.sol
@@ -1,9 +1,8 @@
 // run on Kovan testchain, send some LINK tokens to the contract first
 // source: https://docs.chain.link/docs/get-a-random-number
 
-pragma solidity 0.6.6;
-
-import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/evm-contracts/src/v0.6/VRFConsumerBase.sol";
+pragma solidity ^0.6.0;
+import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract RandomNumberConsumer is VRFConsumerBase {
     bytes32 internal keyHash;
@@ -12,21 +11,21 @@ contract RandomNumberConsumer is VRFConsumerBase {
     
     constructor() 
         VRFConsumerBase(
-            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator https://docs.chain.link/docs/vrf-contracts#kovan
             0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
         ) public
     {
         keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
         fee = 0.1 * 10 ** 18; // 0.1 LINK
     }
-    
-    function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) > fee, "Not enough LINK - fill contract with faucet");
-        return requestRandomness(keyHash, fee, userProvidedSeed);
+    function CheckBalance() public view returns (uint) {
+      return LINK.balanceOf(address(this));
     }
-
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+    function fulfillRandomness(bytes32 /*requestId*/, uint256 randomness) internal override {
         result = randomness;
     }
-    
+    function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
+        require (CheckBalance() >= fee,"Not enough LINK Tokens in contract");
+        return requestRandomness(keyHash, fee, userProvidedSeed);
+    }
 }
--- oracle_examples/chainlink_temperature.sol
+++ oracle_examples/chainlink_temperature.sol
@@ -1,29 +1,40 @@
+// SPDX-License-Identifier: MIT
 // run on Kovan testchain, send some LINK tokens to the contract first
 // based on: https://docs.chain.link/docs/make-a-http-get-request
 // https://docs.chain.link/docs/adapters#httpget
 
-pragma solidity ^0.6.0;
-import "https://raw.githubusercontent.com/smartcontractkit/chainlink/develop/evm-contracts/src/v0.6/ChainlinkClient.sol";
-contract APIConsumer is ChainlinkClient {
-  
-    uint256 public temp;   
-    address private oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
-    bytes32 private jobId = "29fa9aa13bf1468788b7cc4a500a45b8";
-    uint256 private fee = 0.1 * 10 ** 18; // 0.1 LINK
-    
-    constructor() public {
+pragma solidity ^0.7.0;
+import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.7/ChainlinkClient.sol";
+
+contract CheckTemp is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public temp;
+    bytes32 public requestId;
+    address private oracle = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e; // https://docs.chain.link/docs/decentralized-oracles-ethereum-mainnet#kovan
+    bytes32 private jobId = bytes32("29fa9aa13bf1468788b7cc4a500a45b8");
+    uint256 private fee = LINK / 10 ; // 0.1 LINK
+
+    constructor() {
         setPublicChainlinkToken();
+        setChainlinkOracle(oracle);
     }
+          
+   function CheckBalance() public view returns (uint) {
+      LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+      return link.balanceOf(address(this));
+   }
     
-    function requestTemp() public returns (bytes32 requestId){
+    function _callback(bytes32 _requestId, uint256 _result) public recordChainlinkFulfillment(_requestId) { // modifier checks validity
+        temp = _result;
+    }    
+    
+    function requestTemp() public {
+        require (CheckBalance() >= fee,"Not enough LINK Tokens in contract");
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this._callback.selector);
         request.add("get", "http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam");
         request.add("path", "liveweer.0.temp");      // Parse the resulting json        
         request.addInt("times", 1000);               // Multiply the result by 1000 to remove decimals
-        return sendChainlinkRequestTo(oracle, request, fee);
+        requestId=sendChainlinkRequest(request, fee);
     }
-        
-    function _callback(bytes32 _requestId, uint256 _result) public recordChainlinkFulfillment(_requestId){
-        temp = _result;
-    }    
 }

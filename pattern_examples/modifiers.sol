addbe8c2bee5058893e4d778760e10915dc45627
gpersoon <mail@gpersoon.com>
2020-12-07 17:21:23 +0100

    Update modifiers.sol

--- pattern_examples/modifiers.sol
+++ pattern_examples/modifiers.sol
@@ -1,23 +1,23 @@
-pragma solidity ^0.5.12;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
 contract Owned {
     address public owner;
-    uint public creationTime = now;
-    modifier onlyOwner()                { require(msg.sender == owner);    _;}
-    modifier onlyBefore(uint _time)     { require(now < _time);            _;}
-    modifier onlyAfter(uint _time)      { require(now > _time);            _;}
-    modifier onlyBy(address account)    { require(msg.sender == account);  _;}  
-    modifier condition(bool _condition) { require(_condition);             _;}
-    modifier minAmount(uint _amount)    { require(msg.value >= _amount);   _;}
-    constructor() public { owner = msg.sender;}
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0));
-        owner = newOwner;
-    }
+    uint public creationTime = block.timestamp;
+    modifier onlyOwner()                { require(msg.sender == owner,      "Must be owner");       _;}
+    modifier onlyBefore(uint _time)     { require(block.timestamp < _time,  "Too late");            _;}
+    modifier onlyAfter(uint _time)      { require(block.timestamp > _time,  "Too soon");            _;}
+    modifier onlyBy(address account)    { require(msg.sender == account,    "Wrong address" );      _;}  
+    modifier condition(bool _condition) { require(_condition,               "Condition failed");    _;}
+    modifier minAmount(uint _amount)    { require(msg.value >= _amount,     "Not enough ETH send"); _;}
+
+    constructor() { owner = msg.sender;}
+
     function f() payable 
-        onlyAfter(creationTime + 1 minutes) 
         onlyBy(owner) 
         minAmount(2 ether) 
+        onlyAfter(creationTime + 1 minutes) 
         condition(msg.sender.balance >= 50 ether) 
-        public { // code
+        public returns(string memory) { // code
+            return "Done";
     }
 }

f4ffb0aeb5eef873309a6477a915238025f60a8f
gpersoon <mail@gpersoon.com>
2021-03-12 12:58:30 +0100

    erc20

--- token_examples/ERC20Remix.sol
+++ token_examples/ERC20Remix.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.8.0;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
 
     /// @dev Constructor that gives _msgSender() all of existing tokens.
-    constructor () ERC20("TestToken", "Tst") {
+    constructor () ERC20("Web3Examples", "W3ETST") { // max 6 chars
         _mint(msg.sender, 10000 * (10 ** 18));
     }
 }
